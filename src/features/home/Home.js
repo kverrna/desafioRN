a2beaec6b2c22e2de3b8c3dbd8d3d5402d24d463
Ramon Cruz <ramon.cruz.comercial@hotmail.com>
2019-08-21 12:02:33 -0300

    Home

---
+++ src/features/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+
+const Home = () => (
+  <SafeAreaView>
+    <View>
+      <Text>Home</Text>
+    </View>
+  </SafeAreaView>
+);
+
+export default Home;
