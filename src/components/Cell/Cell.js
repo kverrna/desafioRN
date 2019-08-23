0df94237c1d139c03fea2773bf81e8a7b36889a5
Ramon Cruz <ramon.cruz.comercial@hotmail.com>
2019-08-23 09:10:01 -0300

    uso de HOC para navegacao da tela Home para Detalhar

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -2,36 +2,46 @@ import React from 'react';
 import {
   View, Text, Image, TouchableOpacity
 } from 'react-native';
+import { withNavigation } from 'react-navigation';
 import PropTypes from 'prop-types';
 import styles from './Cell.styles';
 import { setaNavegacao } from '../temp';
 
-const Cell = ({
-  urlImageAvatar, title, subtitle, onPress, roundAvatar
-}) => (
-  <TouchableOpacity onPress={onPress} style={styles.cell}>
-    <View style={styles.avatarContainer}>
-      <Image
-        style={(roundAvatar) ? styles.avatarRound : styles.avatar}
-        source={{ uri: urlImageAvatar }}
-      />
-    </View>
-    <View style={styles.containerText}>
-      <Text style={styles.textTitle} numberOfLines={1} ellipsizeMode="tail">{title}</Text>
-      <Text style={styles.textSubtitle}>{subtitle}</Text>
-    </View>
-    <View style={styles.navigationIconContainer}>
-      <Image style={styles.navigationIcon} source={{ uri: setaNavegacao }} />
-    </View>
-  </TouchableOpacity>
-);
 
-Cell.prototype = {
+const Cell = ({
+  urlImageAvatar, title, subtitle, roundAvatar, navigation
+}) => {
+  const navegarParaDetalhe = () => {
+    navigation.navigate('Detail');
+  };
+
+  return (
+    <TouchableOpacity onPress={navegarParaDetalhe} style={styles.cell}>
+      <View style={styles.avatarContainer}>
+        <Image
+          style={(roundAvatar) ? styles.avatarRound : styles.avatar}
+          source={{ uri: urlImageAvatar }}
+        />
+      </View>
+      <View style={styles.containerText}>
+        <Text style={styles.textTitle} numberOfLines={1} ellipsizeMode="tail">{title}</Text>
+        <Text style={styles.textSubtitle}>{subtitle}</Text>
+      </View>
+      <View style={styles.navigationIconContainer}>
+        <Image style={styles.navigationIcon} source={{ uri: setaNavegacao }} />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+Cell.propTypes = {
   urlImageAvatar: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
-  onPress: PropTypes.func.isRequired,
-  roundAvatar: PropTypes.bool.isRequired
+  roundAvatar: PropTypes.bool.isRequired,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired
+  }).isRequired
 };
 
-export default Cell;
+export default withNavigation(Cell);
